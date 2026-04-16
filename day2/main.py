ffc40f30c572bc4e5049d9b0ae9f2c4a5bf49799
JC-PC <qjasonc@gmail.com>
2026-04-16 02:10:31 -0700

    cleaned up day2 main

--- day2/main.py
+++ day2/main.py
@@ -1,14 +1,16 @@
 import random
 
-attempts = 0
-
 def guess(num, ans):
     if ans > num:
-        return print("Too low! Try again!")
+        print("Too low! Try again!")
+        return False
     elif ans < num:
-        return print("Too high! Try again!")
+        print("Too high! Try again!")
+        return False
     else:
-        return print("Correct!")
+        print("Correct!")
+        return True
+
 running = True
 
 while running:
@@ -34,23 +36,25 @@ while running:
         rng = 200
         print(f"You chose {diff} which means you have {attempts} attempts.")
     
-    number = int(input(f"Guess a number between 1 and {rng}. "))
-    attempts -= 1  
+    correct = False
+    attemptNum = 0
 
-    while number != answer and attempts != 0:
-        guess(number, answer)
+    while attempts and not correct:
         number = int(input(f"Guess a number between 1 and {rng}. "))
-        attempts -= 1
+        correct = guess(number, answer)
+        if not correct:
+            attempts -= 1
+        attemptNum += 1
 
-    if attempts <= 0:
-        print(f"That was your last attempt! The correct number was {answer}")
-    else:
+    if correct:
         print(f"Congrats!!! The number was {answer}")
-        print(f"It took you {attempts} attempt(s)")
+        print(f"It took you {attemptNum} attempt(s)")
+    else:
+        print(f"That was your last attempt! The correct number was {answer}")
 
     again = input("Would you like to play again? ").lower()
     while again != "yes" and again != "no":
-        again = input("Would you like to play again?(Yes or No): ").lower
+        again = input("Would you like to play again?(Yes or No): ").lower()
     
     if again == "no":
         running = False
